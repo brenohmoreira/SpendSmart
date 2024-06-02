6a513ce8adde7e78cc5e1ed9757aaaf4bd087d87
Breno <breno.am2004@gmail.com>
2024-06-01 23:07:01 -0300

    style

--- spendsmart-front/src/components/AuthComponent.vue
+++ spendsmart-front/src/components/AuthComponent.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="auth-container">
-    <div id="sign-in" v-if="signIn">
+    <div id="sign-in" v-if="signIn" class="auth-el">
       <form @submit.prevent="authenticate()">
-        <input type="email" id="email" name="email" v-model="email">
-        <input type="password" id="password" name="password" v-model="password">
-        <button> Enviar </button>
+        <label for="email"> E-mail </label>
+        <input type="email" id="email" name="email" v-model="email" placeholder="Digite o e-mail">
+        <label for="password"> Senha </label>
+        <input type="password" id="password" name="password" v-model="password" placeholder="Digite a senha">
+        <button> Conectar-se </button>
       </form>
     </div>
-    <div id="sign-up" v-else>
+    <div id="sign-up" v-else class="auth-el">
 
     </div>
   </div>
@@ -41,8 +43,38 @@
 
 <style scoped>
   #auth-container {
-    width: 60%;
+    width: 30%;
     padding: 20px;
     border-radius: 5px;
+  }
+
+  .auth-el {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .auth-el input {
+    width: 100%;
+    height: 40px;
+    border: 1px solid rgb(143, 143, 143);
+    border-radius: 5px;
+    outline: none;
+    padding: 0 10px;
+    margin-bottom: 10px;
+  }
+
+  .auth-el label {
+    margin-bottom: 4px;
+  }
+
+  .auth-el button {
+    width: 100%;
+    height: 40px;
+    background-color: dodgerblue;
+    color: white;
+    border-radius: 5px;
+    outline: none;
+    border: none;
   }
 </style>
